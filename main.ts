b61059129897f984ce9b077b8b534af77d7d1351
CrisDavid14 <104511716+CrisDavid14@users.noreply.github.com>
2022-04-27 10:38:50 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function on_forever() {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(100)
+})
